ce1b42db947dec6fb14a7debbb1918999c232c3f
kazi Akib Rayhan <kaziakibrayhan2018@gmail.com>
2022-12-31 08:42:56 +0600

    set environment varriable and set up mongodb

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require("express");
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const cors = require("cors");
 require("colors");
+require("dotenv").config();
+
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -10,6 +13,13 @@ app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("genius car server is running!");
+});
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.h5na1rv.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
 });
 
 app.listen(port, () => {
